1741b88dd3b0e52d2c718d147abd18984f1e12ef
jpeg25 <jairo25509@hotmail.com>
2021-11-21 13:26:37 -0500

    Search functionality improved: reset search.

--- 3-cocktails/setup/src/searchForm.js
+++ 3-cocktails/setup/src/searchForm.js
@@ -8,8 +8,12 @@ const input = get(".search-form input");
 form.addEventListener("input", (e) => {
   e.preventDefault();
 
-  const params = input.value;
-  if (!params) return;
+  let params = input.value;
+  if (!params) {
+    //   Para que al borrar la búsqueda se muestren los cócteles defaault en vez
+    //   de los de la última búsqueda. John no hace esto.
+    params = "a";
+  }
 
   const URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${params}`;
   presentDrinks(URL);
